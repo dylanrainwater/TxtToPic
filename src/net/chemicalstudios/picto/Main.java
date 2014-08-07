ae0631f2f9671f6ea5060d7ca1a680a4bae47caa
Chemical Studios <chemicalstudiosdev@gmail.com>
2014-08-07 19:40:48 -0400

    Added pattern mode, Removed PicToTxt mode, Added test.txt test.png test_1.png and test2.png

--- src/net/chemicalstudios/picto/Main.java
+++ src/net/chemicalstudios/picto/Main.java
@@ -1,98 +1,109 @@
 package net.chemicalstudios.picto;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Random;
 import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
 public class Main {
-	
 	private static BufferedImage input = null;
-	private static URL inputDir = null;
-	private static boolean ioMode = false; // Mode of user interaction -- true = user puts in text, false = user inputs in image
 	private static Scanner keyboard = new Scanner(System.in);
-	private static String grassO = "#", waterO = "~", flowerO = "+";
-	private static int grassI = 1, waterI = 2, flowerI = 3;
-	private static int grassRGB = -16711915, waterRGB = -13264129, flowerRGB = -10240;
+	private static int greenI = 1, blueI = 2, redI = 3;
+	private static int greenRGB = new Color(0, 0, 255).getRGB(), blueRGB = new Color(255, 255, 255).getRGB(), redRGB = new Color(255, 0, 0).getRGB();
+	
+	private static String fileName = null;
+	private static String fileNameWithoutExtension = null;
+	private static boolean patternMode = false;
+	private static Random rng = new Random();
+	
+	private static int amount = 0;
 	
 	public static void main(String[] args) {
-		System.out.println("IOMode?");
-		ioMode = keyboard.nextBoolean();
 		
-		if (ioMode) {
-			try {
-				int length = 0, height = 0;
-				keyboard = new Scanner(new File("map2.txt"));
-				
-				length = keyboard.nextInt();
-				height = keyboard.nextInt();
-				System.out.println(length + "x" + height);
-				input = new BufferedImage(length, height, BufferedImage.TYPE_INT_RGB);
-				
-				for (int y = 0; y < height; y++) {
-					for (int x = 0; x < length; x++) {
-						
-						int inText = (keyboard.hasNext()) ? keyboard.nextInt() : 0;
-						
-						System.out.println(x + ", " + y + ": " + inText);
-						if (inText == grassI) {
-							input.setRGB(x, y, grassRGB);
-						} else if (inText == waterI) {
-							input.setRGB(x, y, waterRGB);
-						} else if (inText == flowerI) {
-							input.setRGB(x, y, flowerRGB);
-						} else {
-							input.setRGB(x, y, 0);
-						}
-					}
-				}
-				
-				File outputFile = new File("map2.png");
-				ImageIO.write(input, "png", outputFile);
-				
-				
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		} else {
-			try {
-				input = ImageIO.read(new File("map1.png"));
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
 		
-		if (input != null) {
-			for (int y = 0; y < input.getHeight(); y++) {
-				System.out.println();
-				for (int x = 0; x < input.getWidth(); x++) {
-					if (input.getRGB(x, y) == grassRGB) {
-						System.out.print(grassO);
-					} else if (input.getRGB(x, y) == waterRGB) {
-						System.out.print(waterO);
-					} else if (input.getRGB(x, y) == flowerRGB) {
-						System.out.print(flowerO);
-					} else {
-						System.out.print(0);
-					}
-				}
-			}
-			
+		System.out.println("@FileName - file.txt | file.png\n"
+				+ "@patternMode - true | false\n"
+				+ "@amount - #\n"
+				+ "Input format: FileName,patternMode,amount");
+		String fileArgs[] = keyboard.nextLine().split(",");
+		
+		fileName = fileArgs[0];
+		fileNameWithoutExtension = (String) fileName.subSequence(0, fileName.indexOf("."));
+		patternMode = fileArgs[1].equals("true") ? true : false;
+		amount = (int) Integer.parseInt(fileArgs[2]);
+		
+		
+		if (amount == 0) {
+			System.out.println("Done.");
 		} else {
-			System.out.println("Image was null.");
+			for (int i = 0; i < amount; i++) {
+				if (patternMode) {
+					patternColors();
+				}
+				toPic();
+			}
 		}
 		keyboard.close();
+	}
+	
+	private static void toPic() {
+		try {
+			int length = 0, height = 0;
+			// Read text file inputted by user
+			keyboard = new Scanner(new File(fileName));
+			
+			// first two lines are dimensions
+			length = keyboard.nextInt();
+			height = keyboard.nextInt();
+			
+			// Create the new image and convert the text into each pixel
+			input = new BufferedImage(length, height, BufferedImage.TYPE_INT_RGB);
+			
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < length; x++) {
+					
+					int inText = (keyboard.hasNext()) ? keyboard.nextInt() : 0;
+					
+					if (inText == greenI) {
+						input.setRGB(x, y, greenRGB);
+					} else if (inText == blueI) {
+						input.setRGB(x, y, blueRGB);
+					} else if (inText == redI) {
+						input.setRGB(x, y, redRGB);
+					} else {
+						input.setRGB(x, y, 0);
+					}
+				}
+			}
+			
+			File outputFile = new File(fileNameWithoutExtension + ".png");
+			int fileNum = 0;
+			while (outputFile.exists()) {
+				fileNum++;
+				fileNameWithoutExtension = (String) fileName.subSequence(0, fileName.indexOf(".")) + "_" + fileNum;
+				outputFile = new File(fileNameWithoutExtension + ".png");
+			}
+			
+			// Save image to disk
+			ImageIO.write(input, "png", outputFile);
+			System.out.println("Created file: " + outputFile.getName() + " @ " + outputFile.getAbsolutePath());
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void patternColors() {
+		System.out.println(redRGB + ", " + greenRGB + ", " + blueRGB);
+		redRGB = rng.nextInt() * -16776961;
+		greenRGB = rng.nextInt() * -16776961;
+		blueRGB = rng.nextInt() * -16776961;
 	}
 }
